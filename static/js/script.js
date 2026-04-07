b86046f4ed3a367dbdebcb14450ca40d83b9e7a0
SheikhOsamabinLaden <propve226@gmail.com>
2026-04-07 08:30:10 +0000

    пара 3

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,34 @@
 const input = document.getElementById("searchInput");
 const select = document.getElementById("categorySelect");
 
+let doctors_json = null;
+let occupations_json = null;
+
 function handleChange() {
-const text = input.value;
-const category = select.value;
+    const text = input.value;
+    const category = select.value;
+    console.log(`${text} ${category}`)
+    if (doctors_json == null || occupations_json == null){return}
+    const cards = document.querySelector('.cards');
+    cards.innerHTML = ""
+    for (i in doctors_json)
+    {
+        if (category != 0 && doctors_json[i]["occupation"] != category) { continue }
+        if (text != "" && !doctors_json[i]["name"].toLowerCase().includes(text)) { continue }
+        const newCard = `
+                <div class="card">
+                    <img src="https://placehold.co/600x400/EEE/31343C">
+                    <div class="container">
+                        <p>Ім'я: ${doctors_json[i]["name"]}</p>
+                        <p>Спеціальність: ${occupations_json.find(o => o.id === doctors_json[i]["occupation"])?.name}</p>
+                        <p>Стаж: ${doctors_json[i]["experience"]}</p>
+                        <button class="btn-appointment">Записатися</button>
+                    </div>
+                </div>
+            `;
+        cards.innerHTML += newCard;
+    }
+
 }
 
 input.addEventListener("input", handleChange);
@@ -13,8 +38,8 @@ document.addEventListener('DOMContentLoaded', function() {
     async function load() {
         let url_doctors = `${window.location.href}/api/doctors`;
         let url_occupations = `${window.location.href}/api/occupations`;
-        let doctors_json = await (await fetch(url_doctors)).json();
-        let occupations_json = await (await fetch(url_occupations)).json();
+        doctors_json = await (await fetch(url_doctors)).json();
+        occupations_json = await (await fetch(url_occupations)).json();
         for (i in doctors_json)
         {
             console.log(doctors_json[i]["name"]);
